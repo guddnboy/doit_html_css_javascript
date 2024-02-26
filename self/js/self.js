e284fecc5ce230366f7e6e4bc4f945354babf837
guddnboy <hung3018@naver.com>
2024-02-26 17:00:23 +0900

    study: 나이계산 프로그램

--- self/js/self.js
+++ self/js/self.js
@@ -1,3 +1,5 @@
-const name = prompt('이름을 입력하세요');
-document.write("<div class ='test'><b><big>" + name + "</big></b>님, 환영합니다.</div>");
+const currentYear = 2024;
+const birthYear = prompt('출생년도를 입력하세요. (YYYY)');
+const age = currentYear - birthYear;
 
+document.write('당신의 나이는 현재' + age + '살입니다. (2024년 기준)');
